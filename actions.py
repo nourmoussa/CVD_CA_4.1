40dcde2751c928c3a5ed2fa3e3a223405af12d05
nourmoussa <68866716+nourmoussa@users.noreply.github.com>
2022-02-21 21:17:35 +0000

    Add files via upload

---
+++ actions.py
@@ -0,0 +1,22 @@
+# **************************************************************************************
+# WARNING: This is a static file, useful as a starting point. You may want to change it.
+# **************************************************************************************
+
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for an assistant that schedules reminders and
+# reacts to external events.
+
+# class MyCustomAction(Action):
+#     def name(self) -> Text:
+#         return "action_name"
+#     async def run(
+#         self, dispatcher, tracker: Tracker, domain: Dict[Text, Any],
+#     ) -> List[Dict[Text, Any]]:
+#         return []
+
